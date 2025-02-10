6373b7ecd06f8c077676e82758497eafd4038251
rushi1225 <rushibhingare25@gmail.com>
2025-02-10 21:16:04 +0530

    link and navigate -useRouter using

--- day01/src/app/about/page.jsx
+++ day01/src/app/about/page.jsx
@@ -1,8 +1,15 @@
+import Link from "next/link"
 
 const About = () => {
     return (
         <>
             <h1>About page</h1>
+
+            <Link href='/home'>Go Back Home</Link>
+
+
+
+            
         </>
     )
 }
--- day01/src/app/home/page.jsx
+++ day01/src/app/home/page.jsx
@@ -1,9 +1,26 @@
+'use client'
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+
 
 const Home = () => {
-  return (
-    <>
-      <h1>Home Page</h1>    </>
-  )
+    const router = useRouter();
+    const navigate = (name) => {
+        router.push(name)
+    }
+
+    return (
+        <>
+            <h1>Home Page</h1>
+
+            <Link href='/about'>About Page</Link>
+
+            {/* link and navigation */}
+
+            <button onClick={() => router.push("/about")}> About page </button>
+            <button onClick={() => navigate("/about")}> About page </button>
+        </>
+    )
 }
 
 export default Home
