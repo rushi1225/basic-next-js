c91c4127f9e40a435afa7c2461caf2c4b74d256c
rushi1225 <rushibhingare25@gmail.com>
2025-02-10 22:36:21 +0530

    conditional layout company page hide layout using condition or usePathname

--- day01/src/app/contact/layout.jsx
+++ day01/src/app/contact/layout.jsx
@@ -1,13 +1,47 @@
 
-const layout = ({children}) => {
+//common layout
+
+// import { usePathname } from "next/navigation"
+
+// const layout = ({ children }) => {
+//     const currentpathname = usePathname()
+//     return (
+//         <>
+//             <h1>common layout for contact page</h1>
+//             {
+//                 children
+//             }
+//         </>
+//     )
+// }
+
+
+// conditional layout
+
+
+'use client'
+import { usePathname } from "next/navigation"
+
+const layout = ({ children }) => {
+    const currentpathname = usePathname()
+
     return (
         <>
-            <h1>common layout for contact page</h1>
             {
-                children
+                currentpathname !== '/contact/company' ? <h1>common layout</h1> : null
             }
+            {children}
         </>
     )
 }
+
+
+
+
+
+
+
+
+
 
 export default layout
