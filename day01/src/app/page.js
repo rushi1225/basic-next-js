25187027c5d63f81f61e3dec73a1e0797e575b13
rushi1225 <rushibhingare25@gmail.com>
2025-02-10 19:50:30 +0530

    events function & state

---
+++ day01/src/app/page.js
@@ -0,0 +1,12 @@
+'use client'
+import EventsFunctionState from "@/components/EventsFunctionState";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Rushi</h1>
+      <EventsFunctionState></EventsFunctionState>
+    </>
+  );
+}
---
+++ day01/src/components/EventsFunctionState.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const EventsFunctionState = () => {
+
+    const sayHello = () => {
+        alert("sayhello")
+    }
+
+
+    const [firstname, setFirstname] = useState('rushi')
+
+    const changeName = () => {
+        setFirstname("Rushikesh Bhingare")
+    }
+    return (
+        <>
+            <h1>Events Function And Components</h1>
+            <button onClick={() => alert('hello rushi')}>click event</button>
+            <button onClick={sayHello}>Say Hello</button>
+            <h3>my name is {firstname}</h3>
+            <button onClick={changeName}>change name  State</button>
+        </>
+    )
+}
+
+export default EventsFunctionState
